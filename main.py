50bace734374fb218e32436d77412eaac9fc1121
NikBel523 <belyakov.nkt@gmail.com>
2024-07-25 01:18:56 +0300

    feat: добавлен main.py

---
+++ main.py
@@ -0,0 +1,107 @@
+from models.library import Library
+
+
+def check_if_int(prompt: str) -> int:
+    """Запрашивает у пользователя ввод числа, пока не получит корректное значение."""
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите корректное целое число.")
+
+
+def remove_book(library):
+    """Обрабатывает удаление книги по ID."""
+    book_id = check_if_int("Введите ID книги для удаления: ")
+    library.remove_book(book_id)
+
+
+def add_book(library):
+    """Обрабатывает добавление новой книги."""
+    title = input("Введите название книги: ")
+    author = input("Введите автора книги: ")
+    year = check_if_int("Введите год издания: ")
+    library.add_book(title, author, year)
+
+
+def find_books(library):
+    """Обрабатывает поиск книг по ключевому слову."""
+
+    while True:
+        search_by = input("Искать по (название/автор/год): ").strip().lower()
+        if search_by in ("название", "автор", "год"):
+            break
+        else:
+            print("Ошибка: Пожалуйста, введите 'название', 'автор' или 'год'.")
+
+    if search_by == "год":
+        keyword = check_if_int("Введите год для поиска: ")
+    elif search_by in ("название", "автор"):
+        keyword = input("Введите ключевое слово для поиска: ")
+
+    books = library.find_books(keyword, search_by)
+    if books:
+        for book in books:
+            print(f"ID: {book.id}, Название: {book.title}, Автор: {book.author}, Год: {book.year}, Статус: {book.status}")
+    else:
+        print("Книги не найдены.")
+
+
+def display_books(library):
+    """Обрабатывает отображение всех книг."""
+    library.display_books()
+
+
+def update_status(library):
+    """Обрабатывает изменение статуса книги."""
+    book_id = check_if_int("Введите ID книги для изменения статуса: ")
+    new_status = input("Введите новый статус ('в наличии' или 'выдана'): ").strip()
+    library.update_status(book_id, new_status)
+
+
+def main() -> None:
+    # Инициализация библиотеки.
+    library = Library()
+
+    # Главный цикл
+    while True:
+        print("\nМеню библиотеки:")
+        print("1. Добавить книгу")
+        print("2. Удалить книгу")
+        print("3. Найти книгу")
+        print("4. Отобразить все книги")
+        print("5. Изменить статус книги")
+        print("6. Выход")
+        print("\n")
+
+        choice = input("Выберите действие: ")
+
+        # Обработка выбора действия пользователя
+
+        if choice == "1":
+            add_book(library)
+
+        elif choice == "2":
+            remove_book(library)
+
+        elif choice == "3":
+            find_books(library)
+
+        elif choice == "4":
+            display_books(library)
+
+        elif choice == "5":
+            update_status(library)
+            
+        elif choice == "6":
+            break
+        else:
+            print("Неправильный выбор. Попробуйте снова.")
+
+        if input("\nПродолжить работу (*/нет): ") == "нет":
+            break
+
+
+if __name__ == "__main__":
+    main()
